7e682e47c1dd7a3e8272f2dfb8da641ca3829c68
Dhrona1421 <dhrona1421@gmail.com>
2026-08-03 14:09:27 +0530

    Time: 16 ms (88.92%) | Memory: 16.9 MB (25.24%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for s in strs:
+            key = tuple(sorted(s))
+            groups.setdefault(key, []).append(s)
+
+        return list(groups.values())
