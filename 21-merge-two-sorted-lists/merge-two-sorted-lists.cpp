e67436ecd9e4a4f477b786404a9a49a5f401cdcf
Dhrona1421 <dhrona1421@gmail.com>
2026-08-07 22:08:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (63.58%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        ListNode dummy(0), *cur = &dummy;
+        while (a && b) {
+            if (a->val <= b->val) cur->next = a, a = a->next;
+            else cur->next = b, b = b->next;
+            cur = cur->next;
+        }
+        cur->next = a ? a : b;
+        return dummy.next;
+    }
+};
