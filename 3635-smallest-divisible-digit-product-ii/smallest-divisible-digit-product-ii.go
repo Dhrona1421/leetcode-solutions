40fb71fdfef76438f900ec9e00661bb0ea813ae7
Dhrona1421 <dhrona1421@gmail.com>
2026-08-07 22:20:51 +0530

    Time: 16 ms (87.50%) | Memory: 11.9 MB (12.50%) - LeetSync

--- 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.go
+++ 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.go
@@ -1,132 +1,83 @@
 func smallestNumber(num string, t int64) string {
-	primeCount, isDivisible := getPrimeCount(t)
-	if !isDivisible {
+	temp := t
+	for i := int64(2); i <= 9; i++ {
+		for temp%i == 0 {
+			temp /= i
+		}
+	}
+	if temp > 1 {
 		return "-1"
 	}
 
-	factorCount := getFactorCount(primeCount)
-	if sumValues(factorCount) > len(num) {
-		return construct(factorCount)
+	n := len(num)
+	rem := make([]int64, n+1)
+	rem[0] = t
+	pos := n - 1
+
+	numBytes := []byte(num)
+	for i := 0; i < n; i++ {
+		if numBytes[i] == '0' {
+			pos = i
+			break
+		}
+		rem[i+1] = rem[i] / gcd(rem[i], int64(numBytes[i]-'0'))
 	}
 
-	primeCountPrefix := getPrimeCountFromString(num)
-	firstZeroIndex := strings.Index(num, "0")
-	if firstZeroIndex == -1 {
-		firstZeroIndex = len(num)
-		if isSubset(primeCount, primeCountPrefix) {
-			return num
-		}
+	if rem[n] == 1 {
+		return num
 	}
 
-	for i := len(num) - 1; i >= 0; i-- {
-		d := int(num[i] - '0')
-		primeCountPrefix = subtract(primeCountPrefix, kFactorCounts[d])
-		spaceAfterThisDigit := len(num) - 1 - i
-		if i > firstZeroIndex {
-			continue
-		}
-		for biggerDigit := d + 1; biggerDigit < 10; biggerDigit++ {
-			factorsAfterReplacement := getFactorCount(
-				subtract(subtract(primeCount, primeCountPrefix), kFactorCounts[biggerDigit]),
-			)
-			if sumValues(factorsAfterReplacement) <= spaceAfterThisDigit {
-				fillOnes := spaceAfterThisDigit - sumValues(factorsAfterReplacement)
-				return num[:i] + strconv.Itoa(biggerDigit) + strings.Repeat("1", fillOnes) + construct(factorsAfterReplacement)
+	for i := pos; i >= 0; i-- {
+		for {
+			numBytes[i]++
+			if numBytes[i] > '9' {
+				break
+			}
+			tNow := rem[i] / gcd(rem[i], int64(numBytes[i]-'0'))
+			k := 9
+			for j := n - 1; j > i; j-- {
+				for tNow%int64(k) != 0 {
+					k--
+				}
+				tNow /= int64(k)
+				numBytes[j] = byte('0' + k)
+			}
+			if tNow == 1 {
+				return string(numBytes)
 			}
 		}
 	}
 
-	factorsAfterExtension := getFactorCount(primeCount)
-	return strings.Repeat("1", len(num)+1-sumValues(factorsAfterExtension)) + construct(factorsAfterExtension)
-}
-
-var kFactorCounts = map[int]map[int]int{
-	0: {}, 1: {}, 2: {2: 1}, 3: {3: 1}, 4: {2: 2},
-	5: {5: 1}, 6: {2: 1, 3: 1}, 7: {7: 1}, 8: {2: 3}, 9: {3: 2},
-}
-
-func getPrimeCount(t int64) (map[int]int, bool) {
-	count := map[int]int{2: 0, 3: 0, 5: 0, 7: 0}
-	for _, prime := range []int{2, 3, 5, 7} {
-		for t%int64(prime) == 0 {
-			t /= int64(prime)
-			count[prime]++
+	var ans strings.Builder
+	t = t
+	for i := 9; i > 1; i-- {
+		for t%int64(i) == 0 {
+			ans.WriteByte(byte('0' + i))
+			t /= int64(i)
 		}
 	}
-	return count, t == 1
+
+	ansStr := ans.String()
+	padding := max(n+1-len(ansStr), 0)
+	ansStr += strings.Repeat("1", padding)
+	runes := []rune(ansStr)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
 }
 
-func getPrimeCountFromString(num string) map[int]int {
-	count := map[int]int{2: 0, 3: 0, 5: 0, 7: 0}
-	for _, d := range num {
-		for prime, freq := range kFactorCounts[int(d-'0')] {
-			count[prime] += freq
-		}
+func gcd(a, b int64) int64 {
+	for b != 0 {
+		a, b = b, a%b
 	}
-	return count
+	return a
 }
 
-func getFactorCount(count map[int]int) map[int]int {
-	res := map[int]int{}
-	count8 := count[2] / 3
-	remaining2 := count[2] % 3
-	count9 := count[3] / 2
-	count3 := count[3] % 2
-	count4 := remaining2 / 2
-	count2 := remaining2 % 2
-	count6 := 0
-	if count2 == 1 && count3 == 1 {
-		count2, count3 = 0, 0
-		count6 = 1
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	if count3 == 1 && count4 == 1 {
-		count2 = 1
-		count6 = 1
-		count3, count4 = 0, 0
-	}
-	res[2] = count2
-	res[3] = count3
-	res[4] = count4
-	res[5] = count[5]
-	res[6] = count6
-	res[7] = count[7]
-	res[8] = count8
-	res[9] = count9
-	return res
-}
-
-func construct(factors map[int]int) string {
-	var res strings.Builder
-	for digit := 2; digit < 10; digit++ {
-		res.WriteString(strings.Repeat(strconv.Itoa(digit), factors[digit]))
-	}
-	return res.String()
-}
-
-func isSubset(a, b map[int]int) bool {
-	for key, value := range a {
-		if b[key] < value {
-			return false
-		}
-	}
-	return true
-}
-
-func subtract(a, b map[int]int) map[int]int {
-	res := make(map[int]int, len(a))
-	for k, v := range a {
-		res[k] = v
-	}
-	for k, v := range b {
-		res[k] = max(0, res[k]-v)
-	}
-	return res
-}
-
-func sumValues(count map[int]int) int {
-	sum := 0
-	for _, v := range count {
-		sum += v
-	}
-	return sum
+	return b
 }
