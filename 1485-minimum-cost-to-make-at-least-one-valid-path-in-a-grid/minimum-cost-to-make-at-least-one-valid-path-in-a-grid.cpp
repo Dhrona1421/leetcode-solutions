3ac066754dfd9e6fe5cee2bf9ce5f944aa37f455
Dhrona1421 <dhrona1421@gmail.com>
2026-08-26 14:23:20 -0400

    Time: 5 ms (97.08%) | Memory: 18.7 MB (77.43%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    int minCost(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        deque<pair<int,int>> dq;
-        vector<vector<int>> dist(m, vector<int>(n, 1e9));
+    int minCost(vector<vector<int>>& g) {
+        int m=g.size(), n=g[0].size(), dr[]={0,0,1,-1}, dc[]={1,-1,0,0};
+        deque<pair<int,int>> q{{0,0}};
+        vector<vector<int>> d(m,vector<int>(n,1e9));
+        d[0][0]=0;
 
-        int dr[] = {0, 0, 1, -1};
-        int dc[] = {1, -1, 0, 0};
-
-        dq.push_front({0, 0});
-        dist[0][0] = 0;
-
-        while (!dq.empty()) {
-            auto [r, c] = dq.front();
-            dq.pop_front();
-
-            for (int d = 0; d < 4; d++) {
-                int nr = r + dr[d], nc = c + dc[d];
-
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
-                    continue;
-
-                int cost = dist[r][c] + (grid[r][c] != d + 1);
-
-                if (cost < dist[nr][nc]) {
-                    dist[nr][nc] = cost;
-
-                    if (grid[r][c] == d + 1)
-                        dq.push_front({nr, nc});
-                    else
-                        dq.push_back({nr, nc});
+        while(q.size()){
+            auto [r,c]=q.front(); q.pop_front();
+            for(int k=0;k<4;k++){
+                int x=r+dr[k], y=c+dc[k];
+                if(x<0||x>=m||y<0||y>=n) continue;
+                int w=g[r][c]!=k+1;
+                if(d[r][c]+w<d[x][y]){
+                    d[x][y]=d[r][c]+w;
+                    w?q.push_back({x,y}):q.push_front({x,y});
                 }
             }
         }
-
-        return dist[m - 1][n - 1];
+        return d[m-1][n-1];
     }
 };
